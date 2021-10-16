b525b1a81fdbdd9eb4843a0b0776c6fb811dc93f
JhonnyCamilo <jhonnybeltran57@gmail.com>
2021-10-16 06:49:23 -0500

    first commit

---
+++ src/test/java/jhonny/jhonny/JhonnyApplicationTests.java
@@ -0,0 +1,13 @@
+package jhonny.jhonny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
